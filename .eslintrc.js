ed95131ff8f5b9a6946fe0baca3bfa598305d75f
205277097 <205277097@qq.com>
2024-03-01 21:02:27 +0800

    add

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['plugin:vue/vue3-essential', 'prettier', '@vue/prettier'],
-    rules: {
-      'prettier/prettier': 'error',
-      'vue/multi-word-component-names': ['off']
-    },
-    plugins: ['prettier']
-  };
